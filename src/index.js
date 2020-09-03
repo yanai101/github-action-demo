18ffa4adb6fe40819c1ecb29c66b3f5a2a407fb0
yanai101 <yanai101@gmail.com>
2020-09-04 00:54:55 +0300

    add joke type

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const fetch = require('node-fetch');
+const core = require('@actions/core')
 
 const run = async ()=>{
-    const res = await fetch('https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes');
+    const jokeType = core.getInput('type')
+    console.log(jokeType)
+
+    const res = await fetch(`https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes${jokeType? `/${jokeType}`: ''}`);
     const joke= await res.json();
     console.log(`${joke.type} : ${joke.setup}`)
     console.log(`${joke.punchline}`)
